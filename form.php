ad436f04f780b1824ad3c4b9d0f04f057fd33c55
balserj <60371499+balserj@users.noreply.github.com>
2023-09-29 19:41:39 -0400

    Create form.php

---
+++ form.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<h4>Receive form data and process it </h4>
+
+
+<form action="processIt.php" method="post">
+User Name? <input name="userName" type="text" />
+Password? <input name="passWord" type="text" />
+Name? <input name="Name" type="text" />
+<br>
+<br>
+<br>
+
+<!-- Type a sql query to access mysql db kresman -->
+First query to run? <input name="usersQueryVariable" type="text" />
+<br>
+<br>
+<br>
+Second query to run? <input name="usersQueryVariable2" type="text" />
+<br>
+<input type="submit" />
+
+</form>
+
+</body></html>
